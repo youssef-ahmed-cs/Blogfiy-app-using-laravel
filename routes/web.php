852043f64c785555b90fc874b880842b602185a3
Youssef <youssef419386@fci.bu.edu.eg>
2025-09-10 03:10:49 +0300

    add comments , likes , notifications

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CommentNotificationController;
+use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(CommentController::class)->group(function () {
+    Route::post('posts/{post}/comments', 'store')->name('posts.comments.store');
+    Route::delete('posts/comments/{comment}', 'destroy')->name('comments.destroy');
+});
+
+Route::controller(LikeController::class)->group(function () {
+    Route::post('posts/{post}/like', 'toggleLike')->name('posts.toggleLike');
+});
+
+Route::get('notifications', [CommentNotificationController::class, 'index'])
+    ->name('notifications.index');
