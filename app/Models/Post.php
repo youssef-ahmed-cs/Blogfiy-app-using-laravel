f2d00613da4ead817b962de75e56a1775b505a28
Ahmed Tamer <asdfzch3@gmail.com>
2025-09-10 03:53:57 +0300

    Create Post.php

---
+++ app/Models/Post.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\PostObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+#[ObservedBy([PostObserver::class])]
+class Post extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['title', 'description', 'user_id',
+        'content', 'image', 'views'
+    ];
+
+    public function user_creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        if (!$user) {
+            return false;
+        }
+        return $this->likes()->where('user_id', $user->id)->exists();
+    }
+}
+
